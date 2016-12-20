6d8ef022a083eb8bcac71e254872b8e5f6a12fc4
khrist14n <khrist14n@gmail.com>
2016-12-19 23:27:40 -0500

    app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,41 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+function page(){
+    $(window).bind("load resize", function() {
+        var topOffset = 50;
+        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse');
+            topOffset = 100; // 2-row-menu
+        } else {
+            $('div.navbar-collapse').removeClass('collapse');
+        }
+
+        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
+    });
+}
+function menu(){
+    var url = window.location;
+    var element = $('ul.nav a').filter(function() {
+        return this.href == url || url.href.indexOf(this.href) == 0;
+    }).addClass('active').parent().parent().addClass('in').parent();
+    if (element.is('li')) {
+        element.addClass('active');
+    }
+}
+$(
+    function(){
+        setInterval(
+            function(){
+                $('#side_menu').metisMenu();
+            },
+            1000
+        );
+
+        page();
+        menu();
+    }
+);
