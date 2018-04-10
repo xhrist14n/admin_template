23a3f5adeb13ef87290ba3e9d51499f9cc3517f0
vagrant <vagrant@vagrant.vm>
2018-04-10 08:58:43 +0000

    admin

---
+++ install_database_net.sh
@@ -0,0 +1,17 @@
+bower install --save jquery
+bower install --save jszip
+bower install --save pdfmake
+bower install --save datatables.net-bs
+bower install --save datatables.net-editor-bs
+bower install --save datatables.net-autofill-bs
+bower install --save datatables.net-buttons-bs
+bower install --save datatables.net-colreorder-bs
+bower install --save datatables.net-fixedcolumns-bs
+bower install --save datatables.net-fixedheader-bs
+bower install --save datatables.net-keytable-bs
+bower install --save datatables.net-responsive-bs
+bower install --save datatables.net-rowgroup-bs
+bower install --save datatables.net-rowreorder-bs
+bower install --save datatables.net-scroller-bs
+bower install --save datatables.net-select-bs
+
